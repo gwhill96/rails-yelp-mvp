1ab5f690372dee2c1c90fce4e70ceb93e756ae44
George Hill <gwhill96@gmail.com>
2022-11-16 15:14:51 +0000

    Basic functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+oriental_garden = {name: "Oriental Garden", address: "118A Wandsworth High Street", category: "chinese"}
+taste_of_italy = {name: "Taste of Italy", address: "118 Wandsworth High Street", category: "italian"}
+yo_sushi = {name: "Yo Sushi", address: "124 Wandsworth High Street", category: "japanese"}
+gaucho = {name: "Gaucho", address: "196 Wandsworth High Street", category: "french"}
+bruges = {name: "Bruges", address: "144 Wandsworth High Street", category: "belgian"}
+
+[oriental_garden, taste_of_italy, yo_sushi, gaucho, bruges].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
